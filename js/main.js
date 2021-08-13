bd66180436ea4b5fcbad37237b382180d6641d8d
Anja Tomić <anja.tomic099@gmail.com>
2021-08-13 12:21:48 +0200

    clean code

--- js/main.js
+++ js/main.js
@@ -11,38 +11,42 @@ function loadProducts(){
     fetch('https://my-json-server.typicode.com/ivan-ilic/msg-typicode/products')
     .then(response => response.json())
     .then(data =>{
-        console.log(data)
-        let html = '';
-        data.forEach(product => {
-            html += `
-                <article class="product col-lg-4 col-md-12 d-flex flex-column align-items-center justify-content-center p-3 border border-dark">
-                    <div class="product__icon d-flex align-items-center justify-content-center p-5">
-                        <img src="${product.imageUrl}" alt="${product.name}" class="img-fluid">
-                    </div>
-                    <div class="product__details">
-                        <div class="product__name">
-                            <h3>
-                                ${product.name}
-                            </h3>
-                        </div>
-                        <div class="product__price">
-                            <p>
-                                $${product.price}
-                            </p>
-                        </div>
-                        <div class="addToCart">
-                            <button type="button" class="btn btn-dark add-to-cart-btn" data-id="${product.id}">
-                                Add to Cart
-                            </button>
-                        </div>
-                    </div>
-                </article>
-            `;
-        });
-        productList.innerHTML = html;
+        // console.log(data);
+        printProducts(data);
     })
     .catch(error => {
         alert(`An error occurred` + error);
         console.warn("An error occurred" + error);
     })
+}
+function printProducts(products){
+    // console.log(products);
+    let output = '';
+    products.forEach(product => {
+        output += `
+            <article class="product col-lg-4 col-md-12 d-flex flex-column align-items-center justify-content-center p-3 border border-dark">
+                <div class="product__icon d-flex align-items-center justify-content-center p-5">
+                    <img src="${product.imageUrl}" alt="${product.name}" class="img-fluid">
+                </div>
+                <div class="product__details">
+                    <div class="product__name">
+                        <h3>
+                            ${product.name}
+                        </h3>
+                    </div>
+                    <div class="product__price">
+                        <p>
+                            $${product.price}
+                        </p>
+                    </div>
+                    <div class="addToCart">
+                        <button type="button" class="btn btn-dark add-to-cart-btn" data-id="${product.id}">
+                            Add to Cart
+                        </button>
+                    </div>
+                </div>
+            </article>
+        `;
+    });
+    productList.innerHTML = output;
 }
