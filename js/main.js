12d641371e82cd7380ea157b904d0129c470bfe4
Anja Tomić <anja.tomic099@gmail.com>
2021-08-13 12:02:27 +0200

    dynamic display of products from a JSON file

---
+++ js/main.js
@@ -0,0 +1,48 @@
+// variables and constants
+const productList = document.querySelector('#products__list');
+
+window.addEventListener('DOMContentLoaded', () => {
+    loadProducts();
+});
+
+
+// load product items content from JSON file
+function loadProducts(){
+    fetch('https://my-json-server.typicode.com/ivan-ilic/msg-typicode/products')
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+        let html = '';
+        data.forEach(product => {
+            html += `
+                <article class="product col-lg-4 col-md-12 d-flex flex-column align-items-center justify-content-center p-3 border border-dark">
+                    <div class="product__icon d-flex align-items-center justify-content-center p-5">
+                        <img src="${product.imageUrl}" alt="${product.name}" class="img-fluid">
+                    </div>
+                    <div class="product__details">
+                        <div class="product__name">
+                            <h3>
+                                ${product.name}
+                            </h3>
+                        </div>
+                        <div class="product__price">
+                            <p>
+                                $${product.price}
+                            </p>
+                        </div>
+                        <div class="addToCart">
+                            <button type="button" class="btn btn-dark add-to-cart-btn" data-id="${product.id}">
+                                Add to Cart
+                            </button>
+                        </div>
+                    </div>
+                </article>
+            `;
+        });
+        productList.innerHTML = html;
+    })
+    .catch(error => {
+        alert(`An error occurred` + error);
+        console.warn("An error occurred" + error);
+    })
+}
